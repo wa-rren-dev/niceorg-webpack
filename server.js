19a2397f6754b4672377211c3b267f763cf4eadc
Warren Keith <warrenkeith@gmail.com>
2019-11-19 12:52:51 +0000

    Replace existing express

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const app = express();
 const webpackConfig = require("./webpack.config.js");
 const compiler = webpack(webpackConfig);
 
+var PORT = process.env.PORT || 3000;
+app.set("port", PORT);
+
+// Allow cross origin requests for font files
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath
@@ -15,6 +28,12 @@ app.use(require("webpack-hot-middleware")(compiler));
 
 app.use(express.static("pages"));
 
-app.listen(8080, function() {
-  console.log("\n'/pages' being served at http://localhost:8080\n");
+app.use(function(req, res, next) {
+  res.status(404).sendFile(path.join(__dirname, "./pages/404.html"));
 });
+
+app.listen(app.get("port"), function() {
+  console.log("Express server listening on port " + app.get("port"));
+});
+
+module.exports = app;
